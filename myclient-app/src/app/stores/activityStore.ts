078dd287f0438a2a9d3a144c9b0d9c3070ee1804
MadShark97 <andres.restrepo.1997@gmail.com>
2023-05-24 18:25:10 -1100

    Sixth batch of Front-End changes

--- myclient-app/src/app/stores/activityStore.ts
+++ myclient-app/src/app/stores/activityStore.ts
@@ -15,11 +15,21 @@ export default class ActivityStore {
 		makeAutoObservable(this);
 	}
     
-    //  ===[ COMPUTED PROPERTY ]===
+    //  ===[ COMPUTED FUNCTIONS ]===
     get activitiesByDate() {
         return Array
                 .from(this.activityRegistry.values())
                 .sort((a,b) => Date.parse(a.date) - Date.parse(b.date));
+    }
+    
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as {[key: string]: IActivity[]})
+        )
     }
     
 	//  ===[ ACTIONS ]===
